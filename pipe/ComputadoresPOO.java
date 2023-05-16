43faaae550baf308645dd4407b598905b0b5edf2
AndresBenitez54 <133779157+AndresBenitez54@users.noreply.github.com>
2023-05-16 08:37:24 -0500

    Add files via upload

---
+++ pipe/ComputadoresPOO.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class ClientesPOO here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ComputadoresPOO
+{
+    String identificacion;
+    String nombre;
+    String computador;
+    double costo;
+}
